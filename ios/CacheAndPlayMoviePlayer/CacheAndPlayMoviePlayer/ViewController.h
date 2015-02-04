a086e2a01ba24ed87ce15b362dee368667a8acb0
Hideyuki Takei <takehide22@gmail.com>
2015-02-04 20:55:58 +0900

    First commit.

---
+++ ios/CacheAndPlayMoviePlayer/CacheAndPlayMoviePlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CacheAndPlayMoviePlayer
+//
+//  Created by Hideyuki Takei on 2015/02/04.
+//  Copyright (c) 2015年 EmbodiedWeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios/CacheAndPlayMoviePlayer/CacheAndPlayMoviePlayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CacheAndPlayMoviePlayer
+//
+//  Created by Hideyuki Takei on 2015/02/04.
+//  Copyright (c) 2015年 EmbodiedWeb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
